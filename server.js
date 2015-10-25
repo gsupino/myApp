dd694249ea9e8278fec975dd448c09d7e97b33de
Gianluca Supino <gianluca.supino@gmail.com>
2015-10-25 18:18:45 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    http = require('http')
+
+var app = express();
+
+
+app.get('/', function(req, res) {
+    res.send('You have viewed this page  times!');
+});
+
+http.createServer(app).listen(process.env.PORT || 3000, function() {
+  console.log('Listening on port ' + (process.env.PORT || 3000));
+});
